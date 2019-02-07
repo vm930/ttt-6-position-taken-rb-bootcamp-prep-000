6dc122a934583693c61a51713e7b56298eb7de4e
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 20:26:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+def position_taken?(input)
+  index = input.to_s
+  
+  if board[index] == " "|| board[index] == ""
+    return false
+  elsif
+  
+  
+    return false
+else
+  return true
+  end
+end 
